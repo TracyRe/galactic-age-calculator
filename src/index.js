315a9a4e4d7a70797bdbf72982f11fd48de5d1e2
Tracy Reith <treith@treith.com>
2019-03-15 21:44:26 -0700

    Add text for appropriate age 79 and over, add button to reset entry

--- src/index.js
+++ src/index.js
@@ -7,16 +7,36 @@ $(document).ready(function(){
   $("#galactic-age").submit(function(event) {
     event.preventDefault();
     const birthdate = ($("#birthdate").val());
-    let age = AgePerson(birthdate); // I don't think this is right, it's just a placeholder
-    let output = AgePlanet(age);// I don't think this is right, it's just a placeholder
-    // $(".result").html(`<p>On <em>Mercury</em>, you are <strong>${ageMercury}.</strong></p>
-    // <p>On <em>Venus</em>, you are <strong>${ageVenus}.</strong></p>
-    // <p>On <em>Mars</em>, you are <strong>${ageMars}.</strong></p>
-    // <p>On <em>Jupiter</em>, you are <strong>${ageJupiter}.</strong></p>
-    // <h3>How long do you have to live on each planet?</h3>
-    // <p>On <em>Mercury</em>, you have <strong>${lifeRemainingMercury}</strong> years left.</p>
-    // <p>On <em>Venus</em>, you have <strong>${lifeRemainingVenus}</strong> years left.</p>
-    // <p>On <em>Mars</em>, you have <strong>${lifeRemainingMars}</strong> years left.</p>
-    // <p>On <em>Jupiter</em>, you have <strong>${lifeRemainingJupiter}</strong> years left.</p>`); // placeholder
+    const ageInput = new AgePerson(birthdate);
+    const age = ageInput.ageCalc();
+
+    const ageOutput = new AgePlanet(age);
+    const outputAgeMercury = ageOutput.ageMercury();
+    const outputAgeVenus = ageOutput.ageVenus();
+    const outputAgeMars = ageOutput.ageMars();
+    const outputAgeJupiter = ageOutput.ageJupiter();
+    const outputLifeRemainingMercury = ageOutput.lifeRemainingMercury();
+    const outputLifeRemainingVenus = ageOutput.lifeRemainingVenus();
+    const outputLifeRemainingMars = ageOutput.lifeRemainingMars();
+    const outputLifeRemainingJupiter = ageOutput.lifeRemainingJupiter();
+
+    if (age >= 79) {
+      $(".result").html(`<p>On <em>Mercury</em>, you are <strong>${outputAgeMercury}.</strong> Congratulations on living there <strong>${outputLifeRemainingMercury}</strong> years longer than most.</p>
+      <p>On <em>Venus</em>, you are <strong>${outputAgeVenus}.</strong> You have lived <strong>${outputLifeRemainingVenus}</strong> more years than the average Venuvian transplant.</p>
+      <p>There is <em>Life on Mars</em> and you are <strong>${outputAgeMars}</strong> years old there. You have outlasted most by <strong>${outputLifeRemainingMars}</strong> years.</p>
+      <p>On <em>Jupiter</em>, you are <strong>${outputAgeJupiter} years old.</strong> You have lived <strong>${outputLifeRemainingJupiter}</strong> years longer than average.</p>`);
+    } else {
+      $(".result").html(`<p>On <em>Mercury</em>, you are <strong>${outputAgeMercury}.</strong> You will probably live another <strong>${outputLifeRemainingMercury}</strong> years.</p>
+      <p>On <em>Venus</em>, you are <strong>${outputAgeVenus}.</strong> Enjoy your remaining <strong>${outputLifeRemainingVenus}</strong> years.</p>
+      <p>There is <em>Life on Mars</em> and you are <strong>${outputAgeMars}</strong> years old there. Maybe you'll run into Matt Damon in the next <strong>${outputLifeRemainingMars}</strong> years.</p>
+      <p>On <em>Jupiter</em>, you are <strong>${outputAgeJupiter} years old.</strong> Only <strong>${outputLifeRemainingJupiter}</strong> more to go.</p>`);
+    }
+
+  });
+
+  $("#reset").click(function(event) {
+    event.preventDefault();
+    const birthdate = ($("#birthdate").val(""));
+    $(".result").html("");
   });
 });
