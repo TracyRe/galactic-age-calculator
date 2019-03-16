cf2bffff5571d6881c6ae458ea099a9993004b24
Tracy Reith <treith@treith.com>
2019-03-15 17:00:39 -0700

    Add more code to front end

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ $(document).ready(function(){
   $("#galactic-age").submit(function(event) {
     event.preventDefault();
     const birthdate = ($("#birthdate").val());
-    let age = AgePerson(birthdate);
-    let output = AgePlanet(age);
-    $(".result").html(`<p>${}</p>`);
+    let age = AgePerson(birthdate); // I don't think this is right, it's just a placeholder
+    let output = AgePlanet(age);// I don't think this is right, it's just a placeholder
+    $(".result").html(`<p>On <em>Mercury</em>, you are <strong>${ageMercury}.</strong></p>
+    <p>On <em>Venus</em>, you are <strong>${ageVenus}.</strong></p>
+    <p>On <em>Mars</em>, you are <strong>${ageMars}.</strong></p>
+    <p>On <em>Jupiter</em>, you are <strong>${ageJupiter}.</strong></p>
+    <h3>How long do you have to live on each planet?</h3>
+    <p>On <em>Mercury</em>, you have <strong>${lifeRemainingMercury}</strong> years left.</p>
+    <p>On <em>Venus</em>, you have <strong>${lifeRemainingVenus}</strong> years left.</p>
+    <p>On <em>Mars</em>, you have <strong>${lifeRemainingMars}</strong> years left.</p>
+    <p>On <em>Jupiter</em>, you have <strong>${lifeRemainingJupiter}</strong> years left.</p>`); // placeholder
   });
 });
