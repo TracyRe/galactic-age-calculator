d2bc81c0663bc32e03ff21604ebf4b1d283e1333
Tracy Reith <treith@treith.com>
2019-04-06 13:27:39 -0700

    Refactor to one calculation each for age and life remaing, passing planet as argument

--- src/index.js
+++ src/index.js
@@ -10,34 +10,40 @@ $(document).ready(function(){
     const ageInput = new AgePerson(birthdate);
     const age = ageInput.ageCalc();
 
+    const merc = .24;
+    const venus = .62;
+    const mars = 1.88;
+    const jupiter= 11.86;
+
+
     const ageOutput = new AgePlanet(age);
-    const outputAgeMercury = ageOutput.ageMercury();
-    const outputAgeVenus = ageOutput.ageVenus();
-    const outputAgeMars = ageOutput.ageMars();
-    const outputAgeJupiter = ageOutput.ageJupiter();
-    const outputLifeRemainingMercury = ageOutput.lifeRemainingMercury();
-    const outputLifeRemainingVenus = ageOutput.lifeRemainingVenus();
-    const outputLifeRemainingMars = ageOutput.lifeRemainingMars();
-    const outputLifeRemainingJupiter = ageOutput.lifeRemainingJupiter();
+    const outputAgeMercury = ageOutput.planetAge(merc);
+    const outputAgeVenus = ageOutput.planetAge(venus);
+    const outputAgeMars = ageOutput.planetAge(mars);
+    const outputAgeJupiter = ageOutput.planetAge(jupiter);
+    const outputLifeRemainingMercury = ageOutput.lifeRemaining(merc);
+    const outputLifeRemainingVenus = ageOutput.lifeRemaining(venus);
+    const outputLifeRemainingMars = ageOutput.lifeRemaining(mars);
+    const outputLifeRemainingJupiter = ageOutput.lifeRemaining(jupiter);
 
     $(".result").show();
     if (age >= 79) {
       $(".result").html(`<p>On <em>Mercury</em>, you are <strong>${outputAgeMercury}.</strong> Congratulations on living there <strong>${outputLifeRemainingMercury}</strong> years longer than most.</p>
-      <p>On <em>Venus</em>, you are <strong>${outputAgeVenus}.</strong> You have lived <strong>${outputLifeRemainingVenus}</strong> more years than the average Venuvian transplant.</p>
-      <p>There is <em>Life on Mars</em> and you are <strong>${outputAgeMars}</strong> years old there. You have outlasted most by <strong>${outputLifeRemainingMars}</strong> years.</p>
-      <p>On <em>Jupiter</em>, you are <strong>${outputAgeJupiter} years old.</strong> You have lived <strong>${outputLifeRemainingJupiter}</strong> years longer than average.</p>`);
-    } else {
-      $(".result").html(`<p>On <em>Mercury</em>, you are <strong>${outputAgeMercury}.</strong> You will probably live another <strong>${outputLifeRemainingMercury}</strong> years.</p>
-      <p>On <em>Venus</em>, you are <strong>${outputAgeVenus}.</strong> Enjoy your remaining <strong>${outputLifeRemainingVenus}</strong> years.</p>
-      <p>There is <em>Life on Mars</em> and you are <strong>${outputAgeMars}</strong> years old there. Maybe you'll run into Matt Damon in the next <strong>${outputLifeRemainingMars}</strong> years.</p>
-      <p>On <em>Jupiter</em>, you are <strong>${outputAgeJupiter} years old.</strong> Only <strong>${outputLifeRemainingJupiter}</strong> more to go.</p>`);
-    }
+        <p>On <em>Venus</em>, you are <strong>${outputAgeVenus}.</strong> You have lived <strong>${outputLifeRemainingVenus}</strong> more years than the average Venuvian transplant.</p>
+        <p>There is <em>Life on Mars</em> and you are <strong>${outputAgeMars}</strong> years old there. You have outlasted most by <strong>${outputLifeRemainingMars}</strong> years.</p>
+        <p>On <em>Jupiter</em>, you are <strong>${outputAgeJupiter} years old.</strong> You have lived <strong>${outputLifeRemainingJupiter}</strong> years longer than average.</p>`);
+      } else {
+        $(".result").html(`<p>On <em>Mercury</em>, you are <strong>${outputAgeMercury}.</strong> You will probably live another <strong>${outputLifeRemainingMercury}</strong> years.</p>
+          <p>On <em>Venus</em>, you are <strong>${outputAgeVenus}.</strong> Enjoy your remaining <strong>${outputLifeRemainingVenus}</strong> years.</p>
+          <p>There is <em>Life on Mars</em> and you are <strong>${outputAgeMars}</strong> years old there. Maybe you'll run into Matt Damon in the next <strong>${outputLifeRemainingMars}</strong> years.</p>
+          <p>On <em>Jupiter</em>, you are <strong>${outputAgeJupiter} years old.</strong> Only <strong>${outputLifeRemainingJupiter}</strong> more to go.</p>`);
+        }
 
-  });
+      });
 
-  $("#reset").click(function(event) {
-    event.preventDefault();
-    const birthdate = ($("#birthdate").val(""));
-    $(".result").hide();
-  });
-});
+      $("#reset").click(function(event) {
+        event.preventDefault();
+        const birthdate = ($("#birthdate").val(""));
+        $(".result").hide();
+      });
+    });
--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,101 +1,35 @@
 export class AgePlanet {
   constructor(age) {
     this.age = age;
-    this.factorMerc = .24;
-    this.factorVenus = .62;
-    this.factorMars = 1.88;
-    this.factorJupiter = 11.86;
     this.lifespan = 79;
   }
 
   // calculate age on each planet
-  ageMercury() {
-    const result = this.age / this.factorMerc;
+  planetAge(planet) {
+    const result = this.age / planet;
     const planetAge = result.toFixed(0);
     return planetAge;
   }
-  ageVenus() {
-    const result = this.age / this.factorVenus;
-    const planetAge = result.toFixed(0);
-    return planetAge;
-  }
-
-  ageMars() {
-    const result = this.age / this.factorMars;
-    const planetAge = result.toFixed(0);
-    return planetAge;
-  }
-
-  ageJupiter() {
-    const result = this.age / this.factorJupiter;
-    const planetAge = result.toFixed(0);
-    return planetAge;
-  }
-
   // calculate remaining or exceeded lifespan on each planet
-  lifeRemainingMercury() {
+  lifeRemaining(planet) {
     let remaining = (this.lifespan - this.age);
     if ( remaining < 0 ) {
       const exceeded = Math.abs(remaining);
-      const result = exceeded / this.factorMerc;
+      const result = exceeded / planet;
       const planetLifeExceeded = result.toFixed(0);
       return planetLifeExceeded;
     } else {
-      const result = remaining / this.factorMerc;
-      const planetLifeRemaining = result.toFixed(0);
-      return planetLifeRemaining;
-    }
-  }
-
-  lifeRemainingVenus() {
-    let remaining = (this.lifespan - this.age);
-    if ( remaining < 0 ) {
-      const exceeded = Math.abs(remaining);
-      const result = exceeded / this.factorVenus;
-      const planetLifeExceeded = result.toFixed(0);
-      return planetLifeExceeded;
-    } else {
-      const result = remaining / this.factorVenus;
-      const planetLifeRemaining = result.toFixed(0);
-      return planetLifeRemaining;
-    }
-  }
-
-  lifeRemainingMars() {
-    let remaining = (this.lifespan - this.age);
-    if ( remaining < 0 ) {
-      const exceeded = Math.abs(remaining);
-      const result = exceeded / this.factorMars;
-      let planetLifeExceeded = result.toFixed(0);
-      return planetLifeExceeded;
-    } else {
-      const result = remaining / this.factorMars;
-      let planetLifeRemaining = result.toFixed(0);
-      return planetLifeRemaining;
-    }
-  }
-
-  lifeRemainingJupiter() {
-    let remaining = (this.lifespan - this.age);
-    if ( remaining < 0 ) {
-      const exceeded = Math.abs(remaining);
-      const result = exceeded / this.factorJupiter;
-      const planetLifeExceeded = result.toFixed(0);
-      return planetLifeExceeded;
-    } else {
-      const result = remaining / this.factorJupiter;
+      const result = remaining / planet;
       const planetLifeRemaining = result.toFixed(0);
       return planetLifeRemaining;
     }
   }
 }
 
-
 export class AgePerson {
   constructor(birthdate) {
-  this.birthdate = birthdate;
-  // determine age from birthdate
-
+    this.birthdate = birthdate;
+    // determine age from birthdate
   }
   ageCalc() {
     const today = new Date();
@@ -113,6 +47,4 @@ export class AgePerson {
       return age;
     }
   };
-
-
 }
