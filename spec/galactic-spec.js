3d4c24ed0590285e2a9be9e3a6e69cf95d2b8b4b
Tracy Reith <treith@treith.com>
2019-04-06 14:09:49 -0700

    Rewrite tests for refactored code

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -1,6 +1,11 @@
 import { AgePlanet } from './../src/js/galactic.js';
 import { AgePerson } from './../src/js/galactic.js';
 
+const merc = .24;
+const venus = .62;
+const mars = 1.88;
+const jupiter= 11.86;
+
 // test for calculating persons age from birthdate
 describe('AgePerson', function(){
   it('should return age in years from birthdate', function() {
@@ -14,63 +19,63 @@ describe('AgePerson', function(){
 describe('AgePlanet', function() {
   it('should return age on Mercury', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.ageMercury()).toEqual('150');
+    expect (agePerson.planetAge(merc)).toEqual('150');
   });
 
   it('should return age on Venus', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.ageVenus()).toEqual('58');
+    expect (agePerson.planetAge(venus)).toEqual('58');
   });
 
   it('should return age on Mars', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.ageMars()).toEqual('19');
+    expect (agePerson.planetAge(mars)).toEqual('19');
   });
 
   it('should return age on Jupiter', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.ageJupiter()).toEqual('3');
+    expect (agePerson.planetAge(jupiter)).toEqual('3');
   });
 
 // tests for calculating life remaining/exceeded on each planet
 
   it('should return life remaining on Mercury', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.lifeRemainingMercury()).toEqual('179');
+    expect (agePerson.lifeRemaining(merc)).toEqual('179');
   });
 
   it('should return life remaining on Mercury', function() {
     const agePerson = new AgePlanet(90)
-    expect (agePerson.lifeRemainingMercury()).toEqual('46');
+    expect (agePerson.lifeRemaining(merc)).toEqual('46');
   });
 
   it('should return life remaining on Venus', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.lifeRemainingVenus()).toEqual('69');
+    expect (agePerson.lifeRemaining(venus)).toEqual('69');
   });
 
   it('should return life remaining on Venus', function() {
     const agePerson = new AgePlanet(90)
-    expect (agePerson.lifeRemainingVenus()).toEqual('18');
+    expect (agePerson.lifeRemaining(venus)).toEqual('18');
   });
 
   it('should return life remaining on Mars', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.lifeRemainingMars()).toEqual('23');
+    expect (agePerson.lifeRemaining(mars)).toEqual('23');
   });
 
   it('should return life remaining on Mars', function() {
     const agePerson = new AgePlanet(90)
-    expect (agePerson.lifeRemainingMars()).toEqual('6');
+    expect (agePerson.lifeRemaining(mars)).toEqual('6');
   });
 
   it('should return life remaining on Jupiter', function() {
     const agePerson = new AgePlanet(36)
-    expect (agePerson.lifeRemainingJupiter()).toEqual('4');
+    expect (agePerson.lifeRemaining(jupiter)).toEqual('4');
   });
-  
+
   it('should return life remaining on Jupiter', function() {
     const agePerson = new AgePlanet(90)
-    expect (agePerson.lifeRemainingJupiter()).toEqual('1');
+    expect (agePerson.lifeRemaining(jupiter)).toEqual('1');
   });
 });
